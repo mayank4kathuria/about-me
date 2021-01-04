e16370a21961f17fd4da07dcac22ccc458e9cee9
moka hoka <mayank4kathuria@gmail.com>
2021-01-04 21:51:15 +0530

    Adds React router code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+function Message(props) {
+  return (<div>{props.text || 'just text'}</div>)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path='/page' component={() => <Message text={"Hello another world"} />} />
+        <Route exact path='/' component={ () => <Message text={"Hello world"} />} />
+      </Router>
     </div>
   );
 }
